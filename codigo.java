8e3a1e80488a4ffb36b898fe8e6799ba4ea26442
Miguel Bayon <miguel.bayon@iessanandres.com>
2015-10-01 10:23:07 +0200

    Añadida funcion primo()

--- codigo.java
+++ codigo.java
@@ -17,8 +17,35 @@ public class SyGNumerosPrimos
 	    }
         }
      }
- 
 
+        public static boolean primo(int num)
+        {
+            boolean p = false;
+     
+            if (num < 4) p = true;
+            else
+            {
+            if (num % 2 == 0) p = false; 
+                else
+                {
+                    int contador = 0;  
+                    int i = 1; 
+                    int limite = (num - 1) / 2; 
+                    if  (limite % 2 == 0) limite--;  
+             
+                    while(i <= limite)
+                    {
+                        if (num % i == 0) contador++;
+                            i += 2;
+                            if (contador == 2) i = limite + 1;
+                    }
+ 
+        if (contador == 1) p = true;
+            } 
+        } 
+     
+        return p;
+    }
  
 }
 
