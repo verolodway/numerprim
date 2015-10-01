cf35f61a85f13570bdfc77a1ccea83d359f5438c
Miguel Bayon <miguel.bayon@iessanandres.com>
2015-10-01 10:21:58 +0200

    Commit inicial

---
+++ codigo.java
@@ -0,0 +1,25 @@
+public class SyGNumerosPrimos
+{
+    public static void main(String arg[])
+    {
+        int numDigitos=0;
+        int ndigitos=0;
+        numDigitos=Integer.parseInt(arg[0]);
+        if(numDigitos<=0)
+	{
+            System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
+            for(int i = 1; i <= 99999; i++ )
+            {
+		    ndigitos=contarDigitos(i);
+		    if(ndigitos==numDigitos)
+		        if (primo(i) == true) 
+		            System.out.println(i);    
+	    }
+        }
+     }
+ 
+
+ 
+}
+
+// Miguel Bayon Alonso
